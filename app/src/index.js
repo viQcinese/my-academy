08b75a3a908964662ca07a56689c351223a7200a
viQcinese <vitor.senise@gmail.com>
2025-01-26 23:40:43 -0300

    Initial commit

---
+++ app/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const db = mysql.createConnection({
+  host: process.env.HOST_NAME,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_USER_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("error connecting to the database:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
